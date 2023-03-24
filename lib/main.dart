aa5e95c2339d94bd7871c21b8f89c3f8e563ab22
Elena Dumitru <elena.dumitru0312@stud.acs.upb.ro>
2023-03-24 20:06:23 +0200

    Added email verification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myproject/views/login_view.dart';
 
 import 'firebase_options.dart';
 
@@ -19,6 +20,7 @@ void main() {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+@override
 Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -31,18 +33,39 @@ Widget build(BuildContext context) {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('Verified');
-              } else {
-                print('You need to verify your email');
-              }
-              return const Text ('Done');
+              // final user = FirebaseAuth.instance.currentUser;
+              // if (user?.emailVerified ?? false) {
+              //   return const Text ('Done');
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
             default:
               return const CircularProgressIndicator();
           }
         },
       ),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+        const Text('Please verify your email'),
+        TextButton(onPressed: () async {
+          final user = FirebaseAuth.instance.currentUser;
+          await user?.sendEmailVerification();
+        }, child: const Text('Resend email verification'))
+      ]
     );
   }
 }
