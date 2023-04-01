28ab6b70aec9cf3be04f4781661f8a6739921fcc
Elena Dumitru <elena.dumitru0312@stud.acs.upb.ro>
2023-04-01 18:08:50 +0300

    Add new note route and logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myproject/constants/routes.dart';
 import 'package:myproject/views/login_view.dart';
-import 'package:myproject/views/notes_view.dart';
 import 'package:myproject/views/register_view.dart';
+import 'package:myproject/views/notes/notes_view.dart';
 import 'package:myproject/views/verify_email_view.dart';
+import 'package:myproject/views/notes/new_note_view.dart';
 import 'package:myproject/services/auth/auth_service.dart';
-
-
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ));
 }
