9d8ca227a2332b7365830b25150fdb415fab523d
Elena Dumitru <elena.dumitru0312@stud.acs.upb.ro>
2023-04-12 18:28:35 +0300

    Display individual notes in all notes view

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -14,10 +14,16 @@ class NotesService {
 
   // Making this a singleton class
   static final NotesService _shared = NotesService._sharedInstance();
-  NotesService._sharedInstance();
+  NotesService._sharedInstance() {
+    _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
+      onListen: () {
+        _notesStreamController.sink.add(_notes);
+      }
+    );
+  }
   factory NotesService() => _shared;
 
-  final _notesStreamController = StreamController<List<DatabaseNote>>.broadcast();
+  late final StreamController<List<DatabaseNote>> _notesStreamController;
 
   Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
 
