5a2b71694728e1b50476184dc9c7afccbdea4cf4
Elena Dumitru <elena.dumitru0312@stud.acs.upb.ro>
2023-04-27 18:41:04 +0300

    Add glass box effect class

---
+++ lib/styles/glass_box.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+// ignore_for_file: use_key_in_widget_constructors, prefer_typing_uninitialized_variables
+
+
+final _borderRadius = BorderRadius.circular(30);
+
+class GlassBox extends StatelessWidget {
+
+  final double height;
+  final double width;
+  final child;
+
+  const GlassBox({
+    required this.width,
+    required this.height,
+    required this.child
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: _borderRadius,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: Stack(
+          children: [
+            // blur effect
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 2,
+                sigmaY: 2
+              ),
+              child: Container(),
+            ),
+
+            // gradient effect
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white.withOpacity(0.1)),
+                borderRadius: _borderRadius,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.white.withOpacity(0.4),
+                    Colors.white.withOpacity(0.1),
+                  ],
+                ),
+              ),
+            ),
+
+            // child
+            Center(
+              child: child,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
